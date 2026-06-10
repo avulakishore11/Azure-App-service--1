1f8bb1181e446c402f82d7416a28d51116b51524
avulakishore11 <avulakishore99@gmail.com>
2026-06-10 08:27:29 -0400

    fixed: add azuread auth is true in backedn

--- backend.tf
+++ backend.tf
@@ -3,6 +3,7 @@ terraform {
     resource_group_name  = "tgts-tfstatedev-01"
     storage_account_name = "saterrastatedev02"
     container_name       = "tfstate"
+    use_azuread_auth     = true
     # key is injected per-environment by the pipeline (TerraformTaskV4 backendAzureRmKey)
   }
 }
