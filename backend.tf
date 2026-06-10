9827589c21c0de0055db76b3d14e4b76cb3c2fb7
avulakishore11 <avulakishore99@gmail.com>
2026-06-10 07:56:28 -0400

    fixed: storage account and resource group for terraform backend in azure-pipelines.yml and backend.tf

--- backend.tf
+++ backend.tf
@@ -1,3 +1,8 @@
 terraform {
-  backend "azurerm" {}
+  backend "azurerm" {
+    resource_group_name  = "tgts-tfstatedev-01"
+    storage_account_name = "saterrastatedev02"
+    container_name       = "tfstate"
+    # key is injected per-environment by the pipeline (TerraformTaskV4 backendAzureRmKey)
+  }
 }
